f97466a0909723ecaf203b1d277040ac8005c4b8
Shea Belsky <shb95@cornell.edu>
2018-03-13 00:30:16 -0400

    Better directory specification

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -51,19 +51,31 @@ module.exports = {
             },
             {
                 test: /\.(woff|woff2|eot|ttf)$/,
-                loader: "url-loader?limit=100000&name=./css/[hash].[ext]"
+                use: [
+                    {
+                        loader: "url-loader?limit=100000",
+                        options: {
+                            name: "css/[hash].[ext]"
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 use: [
-                    "file-loader?limit=100000&name=./img/[hash].[ext]",
+                    {
+                        loader: "file-loader?limit=100000",
+                        options: {
+                            name: "img/[hash].[ext]"
+                        }
+                    },
                     "img-loader"
                 ]
             },
             {
                 test: /\.(ico|pdf)$/i,
                 use: [
-                    "file-loader?name=[name].[ext]"
+                    "file-loader?name=img/[name].[ext]"
                 ]
             }
         ],
