b7c039ccc512dfb8d63494d53fe5c0dd11cc7bbe
Shea Belsky <shb95@cornell.edu>
2018-03-13 00:08:52 -0400

    Maybe this time

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -49,7 +49,6 @@ module.exports = {
     entry: `${__dirname}/src/index.js`,
     output: {
         path: `${__dirname}/docs`,
-        publicPath: "/",
         filename: "js/bundle.js",
     },
     module: {
