bc6904e49bc98e8398cf35069c806ff008c390e8
Shea Belsky <sheabelsky@gmail.com>
2018-09-11 21:31:05 -0400

    Formatting fixes

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -23,6 +23,9 @@ export default function () {
                             prototyping, design tools, and infrastructure for designers,
                             researchers, and other User Experience Engineers.
                         </p>
+                        <p>
+                            See my <a href="https://uxdesign.cc/user-experience-engineering-the-adventure-from-student-to-google-intern-cef990125493" rel="noopener noreferrer" target="_blank">Medium blog post</a> on my internship experience!
+                        </p>
                     </div>
                     <div className="work-pic" />
                 </div>
@@ -34,17 +37,15 @@ export default function () {
                         <h2>IBM Watson</h2>
                         <h3>Front End Web Developer Intern</h3>
                         <p>
-                            <u>Summer 2016:</u> Implemented functionality for user input through speech
-                            for the <a href="https://conversation-demo.mybluemix.net/"> Watson Conversation
-                            demo app</a>.
-                            {/* Check out the <a href="/watson" target="_blank">presentation I gave</a> on how Watson Conversation works! */}
-                        </p>
-                        <p>
                             <u>Summer 2017:</u> With the Advanced Client Technology & Solutions team,
                             I developed a video summarization algorithm, API, and client using Watson
                             Developer Cloud services and offerings.
                         </p>
-
+                        <p>
+                            <u>Summer 2016:</u> Implemented functionality for user input through speech
+                            for the <a href="https://conversation-demo.mybluemix.net/" rel="noopener noreferrer" target="_blank"> Watson Conversation
+                            demo app</a>.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -52,12 +53,14 @@ export default function () {
                 <div className="work-container">
                     <div className="work-desc">
                         <h2>Ezra Tech</h2>
-                        <h3>Chief Information Officer & Chief Technology Officer</h3>
-                        <a href="https://www.ezratech.us" target="_blank">Ezra Tech</a> is a full-stack
-                        solution for Science Olympiad event management. Created & maintain client experience
-                        and server-side processing to aid tournament organizers, coaches, and participants
-                        with event logistics. Made with Node.js, Express.js, MongoDB, PugJS, and Semantic
-                        UI.
+                        <h3>Co-Founder, CTO, and CIO</h3>
+                        <p>
+                            <a href="https://www.ezratech.us" rel="noopener noreferrer" target="_blank">Ezra Tech</a> is a full-stack
+                            solution for Science Olympiad event management. Created & maintain client experience
+                            and server-side processing to aid tournament organizers, coaches, and participants
+                            with event logistics. Made with Node.js, Express.js, MongoDB, PugJS, and Semantic
+                            UI.
+                        </p>
                     </div>
                     <div className="work-pic" />
                 </div>
@@ -68,11 +71,13 @@ export default function () {
                     <div className="work-desc">
                         <h2>Cornell Design & Tech Initiative</h2>
                         <h3>Former Developer Lead</h3>
-                        <a href="http://cornelldti.org" target="_blank">Cornell DTI</a> is a group of
-                        students who are passionate about, making a change in our community through
-                        design and technology. As Developer Lead, I coordinated front end developers
-                        across all DTI projects, aided individual projects on an ad-hoc basis, interviewed
-                        potential front end developers, and onboarded new project team members.
+                        <p>
+                            <a href="http://cornelldti.org" rel="noopener noreferrer" target="_blank">Cornell DTI</a> is a group of
+                            students who are passionate about, making a change in our community through
+                            design and technology. As Developer Lead, I coordinated front end developers
+                            across all DTI projects, aided individual projects on an ad-hoc basis, interviewed
+                            potential front end developers, and onboarded new project team members.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -81,13 +86,14 @@ export default function () {
                     <div className="work-desc">
                         <h2>Big Red Hacks</h2>
                         <h3>Web Team Lead</h3>
-                        <a href="https://bigredhacks.com" target="_blank">Big Red Hacks</a> is Cornell
-                        University's first hackathon. As the Web Team Lead, I work with other team members
-                        to plan and implement essential functionality and software for our website.
-                        I am also the first point of contact to address issues that are identified on
-                        GitHub and in Slack. On the day of the event, I support hackers and event logistics
-                        by resolving any critical website errors. I am currently leading a rebuild of
-                        our website with React, PostgreSQL, Node.js, and Express.
+                        <p>
+                            <a href="https://bigredhacks.com" rel="noopener noreferrer" target="_blank">Big Red Hacks</a> is Cornell
+                            University's first hackathon. As the Web Team Lead, I work with other team members
+                            to plan and implement essential functionality and software for our website.
+                            I am also the first point of contact to address issues that are identified on
+                            GitHub and in Slack. On the day of the event, I support hackers and event logistics
+                            by resolving any critical website errors.
+                        </p>
                     </div>
                     <div className="work-pic" />
                 </div>
@@ -98,52 +104,15 @@ export default function () {
                     <div className="work-desc">
                         <h2>Information Science Dept.</h2>
                         <h3>Teaching Assistant</h3>
-                        Responsibilities include holding walk-in office hours, grading student assignments,
-                        addressing questions using Piazza (online class management software) and teaching
-                        a 20-student discussion section (1300/2300). Head TA for 4340, aiding professor
-                        with syllabus and course development, organizing undergraduate TAs, and assisting
-                        in lectures.
-                        <table id="ta-table">
-                            <thead>
-                                <tr>
-                                    <th>Course #</th>
-                                    <th>Course Name</th>
-                                    <th>Term</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>INFO/CS 1300</td>
-                                    <td>Introductory Web Design & Programming</td>
-                                    <td>Fall 15/16</td>
-                                </tr>
-                                <tr>
-                                    <td>INFO/CS 2300</td>
-                                    <td>Intermediate Web Design & Programming</td>
-                                    <td>Spring 16</td>
-                                </tr>
-                                <tr>
-                                    <td>INFO/CS 3300</td>
-                                    <td>Data-Driven Web Applications</td>
-                                    <td>Spring 17</td>
-                                </tr>
-                                <tr>
-                                    <td>INFO/STS 4240</td>
-                                    <td>Designing Technology for Social Impact</td>
-                                    <td>Fall 16/17</td>
-                                </tr>
-                                <tr>
-                                    <td>INFO 4310</td>
-                                    <td>Interactive Information Visualization</td>
-                                    <td>Spring 18</td>
-                                </tr>
-                                <tr>
-                                    <td>INFO 4340</td>
-                                    <td>User Experience and Software Development Studio</td>
-                                    <td>Fall 2018</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <p>
+                            Responsibilities include holding walk-in office hours, grading student
+                            assignments, addressing questions using Piazza and teaching a 20-student
+                            discussion section. Undergraduate TA for INFO/CS 1300, 2300, 3300, and 4310.
+                        </p>
+                        <p>
+                            Head TA for 4340, aiding professor with syllabus and course development,
+                            organizing undergraduate TAs, and assisting in lectures.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -152,17 +121,20 @@ export default function () {
                     <div className="work-desc">
                         <h2>Slope Day Programming Board</h2>
                         <h3>Recruitment Director (May 2017 - May 2018)</h3>
-                        <a href="http://slopeday.cornell.edu" target="_blank">Slope Day</a> is Cornell
-                        University's largest music event, attended by thousands of people. As the
-                        Recruitment Director, I redesigned the Slope Day website (in coordination with
-                        the Branding Director) with new branding, information, and flavor. I also distributed
-                        advertisements to the Cornell student body, organized committees for general
-                        body members to participate in, and managed our email list.
+                        <p>
+                            <a href="http://slopeday.cornell.edu" rel="noopener noreferrer" target="_blank">Slope Day</a> is Cornell
+                            University's largest music event, attended by thousands of people. As the
+                            Recruitment Director, I redesigned the Slope Day website with new branding,
+                            information, and flavor. I also distributed advertisements to the Cornell
+                            student body, organized general body committees to participate in, and managed our email list.
+                        </p>
                         <h3>Volunteer Director (May 2016 - May 2017)</h3>
-                        Director in charge of volunteers for Slope Day festival, including Cornell students,
-                        faculty, and staff. Recruited volunteers and coordinated their duties, met with
-                        other volunteer leads, and discussed logistics with the rest of the Slope Day
-                        Programming Board.
+                        <p>
+                            Director in charge of volunteers for Slope Day festival, including Cornell students,
+                            faculty, and staff. Recruited volunteers and coordinated their duties, met with
+                            other volunteer leads, and discussed logistics with the rest of the Slope Day
+                            Programming Board.
+                        </p>
                     </div>
                     <div className="work-pic" />
                 </div>
@@ -173,18 +145,19 @@ export default function () {
                     <div className="work-desc">
                         <h2>Information Science Student Association</h2>
                         <h3>Advisor (May 2017 - Present)</h3>
-                        Aided other executive board members of ISSA with event planning, presence, and
-                        other ISSA functions. Planned, organized, and collaborated with other CIS community
-                        organizations for a
-                        <a href="http://cornellsun.com/2017/11/07/pollack-addresses-increasing-enrollment-in-computer-science-classes/">
-                        guest talk by Cornell President Martha Pollack
-                        </a>.
+                        <p>
+                            Aided other executive board members of ISSA with event planning, presence, and
+                            other ISSA functions. Planned, organized, and collaborated with other CIS community
+                            organizations for a <a href="http://cornellsun.com/2017/11/07/pollack-addresses-increasing-enrollment-in-computer-science-classes/" rel="noopener noreferrer" target="_blank">guest talk by Cornell President Martha Pollack</a>.
+                        </p>
                         <h3>President (December 2016 - May 2017)</h3>
-                        Presided at general body and executive board meetings, assisted event chairs with
-                        logistics such as food/room reservation, communicated with guests and faculty,
-                        and managed online presence. The President also interacted with members of faculty
-                        and staff to increase club presence, and provided outreach to new and prospective
-                        Information Science students.
+                        <p>
+                            Presided at general body and executive board meetings, assisted event chairs with
+                            logistics such as food/room reservation, communicated with guests and faculty,
+                            and managed online presence. The President also interacted with members of faculty
+                            and staff to increase club presence, and provided outreach to new and prospective
+                            students.
+                        </p>
                     </div>
                 </div>
             </div>
