8f52f134d7e26e3d456251b1dbc78d4b93436a70
Shea Belsky <sheabelsky@gmail.com>
2019-01-08 08:53:05 -0700

    No lazy load, better page transitions
    It's either lazy load or page transitions. Going with page transitions.

--- src/components/pages/work/DTI.js
+++ src/components/pages/work/DTI.js
@@ -9,7 +9,7 @@ function DTI() {
             title="Cornell Design & Tech Initiative"
         >
             <h2>Lead Instructor and Creator of "Trends in Web Development" Course</h2>
-            <h3>April 2018 - Present</h3>
+            <h3>April - December 2018</h3>
             <p>
                 A weekly course introducing industry standard web technologies, including React, Node.js, Express, JavaScript ES6, Webpack, Babel, and more. I am the creator and lead instructor for this student-led course, developing the curriculum, slides, and assignments, and working with Cornell administration and faculty.
             </p>
--- src/components/pages/work/brh.js
+++ src/components/pages/work/brh.js
@@ -9,7 +9,7 @@ function BigRedHacks() {
             title="BigRed//Hacks"
         >
             <h2>Web Team Lead</h2>
-            <h3>May 2016 - Present</h3>
+            <h3>May 2016 - December 2018</h3>
             <p>
                 <a href="https://bigredhacks.com" rel="noopener noreferrer" target="_blank">Big Red Hacks</a> is Cornell University's first hackathon. As the Web Team Lead, I work with other team members to plan and implement essential functionality and software for our website. I am also the first point of contact to address issues that are identified on GitHub and in Slack. On the day of the event, I support hackers and event logistics by resolving any critical website errors.
             </p>
