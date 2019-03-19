b87c4c4d9f06292c25e1f23d1bfd66727d98a71a
Shea Belsky <sheabelsky@gmail.com>
2019-03-18 23:17:42 -0400

    Start of Wayfair update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ const plugins = [
         },
     }),
     new SWPrecacheWebpackPlugin({
-        cacheId: "shea-belsky-website-1.2.5",
+        cacheId: "shea-belsky-website-1.2.6",
         dontCacheBustUrlsMatching: /\.\w{8}\./,
         filename: "js/service-worker.js",
         logger(message) {
