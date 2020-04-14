78faa059cdbe060641c5c2b7d8b427ea371566a2
Shea Belsky <sheabelsky@gmail.com>
2020-04-13 23:03:33 -0400

    Fixing photography page

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ const plugins = [
         },
     }),
     new SWPrecacheWebpackPlugin({
-        cacheId: "shea-belsky-website-1.3.2",
+        cacheId: "shea-belsky-website-1.3.3",
         dontCacheBustUrlsMatching: /\.\w{8}\./,
         filename: "js/service-worker.js",
         logger(message) {
