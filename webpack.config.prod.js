ad44d0fb4b3d7eedc9d4ab28f1dd623dca350855
Shea Belsky <sheabelsky@gmail.com>
2020-09-23 22:08:55 -0400

    Update resume

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ const plugins = [
         },
     }),
     new SWPrecacheWebpackPlugin({
-        cacheId: "shea-belsky-website-1.4.0",
+        cacheId: "shea-belsky-website-1.4.1",
         dontCacheBustUrlsMatching: /\.\w{8}\./,
         filename: "js/service-worker.js",
         logger(message) {
