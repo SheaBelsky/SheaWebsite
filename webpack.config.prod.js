817e01d370565cac30f8bc105899b032f370bbe3
Shea Belsky <sheabelsky@gmail.com>
2021-05-06 22:42:21 -0400

    Fix a few stray bugs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ const plugins = [
         },
     }),
     new SWPrecacheWebpackPlugin({
-        cacheId: "shea-belsky-website-1.4.6",
+        cacheId: "shea-belsky-website-1.4.7",
         dontCacheBustUrlsMatching: /\.\w{8}\./,
         filename: "js/service-worker.js",
         logger(message) {
