9c22c8d050ee1c103250fefd4ee682c9fc19bbd3
Shea Belsky <sheabelsky@gmail.com>
2021-11-13 10:49:52 -0500

    Fix typo

--- src/components/pages/work/Hubspot.js
+++ src/components/pages/work/Hubspot.js
@@ -9,7 +9,7 @@ function HubSpot() {
             title="HubSpot"
         >
             <section>
-                <h2>Senior Software Enginer I</h2>
+                <h2>Senior Software Engineer I</h2>
                 <h3>June 2021 - Present</h3>
                 <p>
                     I currently work for HubSpot as a Senior Software Engineer I. I'm a frontend engineer for the Social team, connecting HubSpot's customers and companies with social media analytics and putting the power of social media at their fingertips.
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,7 +20,7 @@ const plugins = [
         },
     }),
     new SWPrecacheWebpackPlugin({
-        cacheId: "shea-belsky-website-1.5.1",
+        cacheId: "shea-belsky-website-1.5.2",
         dontCacheBustUrlsMatching: /\.\w{8}\./,
         filename: "js/service-worker.js",
         logger(message) {
